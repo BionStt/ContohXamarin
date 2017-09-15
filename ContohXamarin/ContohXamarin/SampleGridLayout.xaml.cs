63f718766f3678d11aa077d3bb61137c57a015c9
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-15 13:36:27 +0700

    menambahkan aplikasi calculator

--- ContohXamarin/ContohXamarin/SampleGridLayout.xaml.cs
+++ ContohXamarin/ContohXamarin/SampleGridLayout.xaml.cs
@@ -15,6 +15,37 @@ namespace ContohXamarin
         public SampleGridLayout()
         {
             InitializeComponent();
+
+            btnTambah.Clicked += BtnHitung_Clicked;
+            btnKurang.Clicked += BtnHitung_Clicked;
+            btnKali.Clicked += BtnHitung_Clicked;
+            btnBagi.Clicked += BtnHitung_Clicked;
+        }
+
+        private void BtnHitung_Clicked(object sender, EventArgs e)
+        {
+            double bil1 = Convert.ToDouble(txtBil1.Text);
+            double bil2 = Convert.ToDouble(txtBil2.Text);
+            double hasil;
+
+            var myBtn = (Button)sender;
+            if (myBtn.Text == "Tambah")
+            {
+                hasil = bil1 + bil2;
+            }
+            else if (myBtn.Text == "Kurang")
+            {
+                hasil = bil1 - bil2;
+            }
+            else if(myBtn.Text == "Kali")
+            {
+                hasil = bil1 * bil2;
+            }
+            else
+            {
+                hasil = bil1 / bil2;
+            }
+            txtHasil.Text = hasil.ToString();
         }
     }
 }
