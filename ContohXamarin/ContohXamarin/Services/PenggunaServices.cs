7777a23e9034e58929f9b0a0bce83bead4104ffb
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-16 12:24:18 +0700

    services untuk login

--- ContohXamarin/ContohXamarin/Services/PenggunaServices.cs
+++ ContohXamarin/ContohXamarin/Services/PenggunaServices.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net.Http;
+using ContohXamarin.Models;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ContohXamarin.Services
 {
@@ -19,6 +22,19 @@ namespace ContohXamarin.Services
             _client = new HttpClient();
         }
 
-        public async Task<bool> Login()
+        public async Task<bool> Login(Pengguna pengguna)
+        {
+            var strUri = new Uri(Path.Combine(backendUrl, "api/Pengguna/api/Pengguna/Login"));
+            var jsonPengguna = JsonConvert.SerializeObject(pengguna);
+            var content = new StringContent(jsonPengguna, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PostAsync(strUri, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                bool isValid = Convert.ToBoolean(result);
+                return isValid;
+            }
+            return false;
+        }
     }
 }
