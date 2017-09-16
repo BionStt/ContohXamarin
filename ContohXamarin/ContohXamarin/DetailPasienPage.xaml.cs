c453704f8afd45cae09f6ca89a55ab8e49a45299
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-16 16:04:57 +0700

    delete update

--- ContohXamarin/ContohXamarin/DetailPasienPage.xaml.cs
+++ ContohXamarin/ContohXamarin/DetailPasienPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ContohXamarin.Models;
+using ContohXamarin.Services;
 
 namespace ContohXamarin
 {
@@ -15,6 +17,54 @@ namespace ContohXamarin
         public DetailPasienPage()
         {
             InitializeComponent();
+        }
+
+        private async void btnEdit_Clicked(object sender, EventArgs e)
+        {
+            var editPasien = new Pasien
+            {
+                Nama = txtNama.Text,
+                Alamat = txtAlamat.Text,
+                Telpon = txtTelpon.Text,
+                Umur = Convert.ToInt32(txtUmur.Text),
+                PasienID = Convert.ToInt32(txtPasienID.Text)
+            };
+
+            PasienServices pasienServices = new PasienServices();
+            try
+            {
+                await pasienServices.UpdatePasien(editPasien);
+                await DisplayAlert("Keterangan", "Data Pasien berhasil diedit", "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private async void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            var deletePasien = new Pasien
+            {
+                Nama = txtNama.Text,
+                Alamat = txtAlamat.Text,
+                Telpon = txtTelpon.Text,
+                Umur = Convert.ToInt32(txtUmur.Text),
+                PasienID = Convert.ToInt32(txtPasienID.Text)
+            };
+
+            PasienServices pasienServices = new PasienServices();
+            try
+            {
+                await pasienServices.DeletePasien(deletePasien);
+                await DisplayAlert("Keterangan", "Data Pasien berhasil didelete", "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
         }
     }
 }
