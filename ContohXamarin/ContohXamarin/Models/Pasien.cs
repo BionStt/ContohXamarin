f58e87a8dd521cf2556bfbee0f84fedf67a3609b
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-16 12:11:07 +0700

    menambahkan model pengguna dan pasien

---
+++ ContohXamarin/ContohXamarin/Models/Pasien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContohXamarin.Models
+{
+    public class Pasien
+    {
+        public int PasienID { get; set; }
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public int Umur { get; set; }
+        public string Telpon { get; set; }
+    }
+}
---
+++ ContohXamarin/ContohXamarin/Services/PenggunaServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+
+namespace ContohXamarin.Services
+{
+    public class PenggunaServices
+    {
+        private const string backendUrl = "http://actualwebapi.azurewebsites.net/";
+        private HttpClient _client;
+
+        public PenggunaServices()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<bool> Login()
+    }
+}
